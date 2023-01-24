7375197e5305197f51f1ef0aaca08f30fe14061b
Aleksandr Dronov <dronovspb@gmail.com>
2023-01-24 20:21:29 +0600

    JSX

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-// Чтобы работал JSX, React должен быть в замыкании
+// React должен находиться в области видимости JSX-файла
 import React from "./react";
 import ReactDom from "./react-dom";
-import Counter from "./counter";
-
-// Функциональный компонент
-const Greeting = ({ name }) => <p>Привет, {name}!</p>;
-
-const foods = [
-  'Банановый чизкейк с семенами чиа',
-  'Картошка обжаренная на курдюке с луком и котлетой из копченого лосося',
-  'Овсяная каша с мёдом, орехами и фруктами'
-]
 
 const App = (
   <div>
@@ -18,13 +8,10 @@ const App = (
       Да здравствует React!
     </h1>
     <p>Самостоятельная реализация React в 90 строк кода</p>
-    <Greeting name={"Самодельный React"} />
-    <Counter />
-    <h4>Сегодня в меню:</h4>
-    <ul>
-      {foods.map(food => <li>{food}</li>)}
-    </ul>
   </div>
 );
 
-ReactDom.render(App, document.getElementById("root"));
+ReactDom.render(App, document.getElementById('root'));
+
+
+
--- src/react-dom.js
+++ src/react-dom.js
@@ -1,24 +1,14 @@
 import * as snabbdom from 'snabbdom';
 import propsModule from 'snabbdom/modules/props';
-import eventListenersModule from 'snabbdom/modules/eventlisteners';
-import React from './react';
 
-// propsModule -> отвечает за модификацию текстовых атрибутов
-// eventListenersModule -> отвечает за обработку событий на элементах
-const reconcile = snabbdom.init([propsModule, eventListenersModule]);
+const reconcile = snabbdom.init([propsModule]);
 
 // Переменная, содержащая корневой элемент, который функция render вернула последним
-let rootVNode;
+let rootVNode = null;
 
-// React.render(<App />, document.getElementById('root'));
-// el -> <App />
-// rootDomElement -> document.getElementById('root')
 const render = (el, rootDomElement) => {
-  // В этой функции описывается механизм размещения элемента el в указанном узле rootDomElement
-  // Например, ReactDom.render(<App />, document.getElementById('root'));
-
   // Этот блок кода будет вызван при первом вызове функции render
-  if (rootVNode == null) {
+  if (rootVNode === null) {
     rootVNode = rootDomElement;
   }
 
@@ -26,21 +16,7 @@ const render = (el, rootDomElement) => {
   rootVNode = reconcile(rootVNode, el);
 }
 
-// ReactDom указывает React как обновлять DOM
-React.__updater = (componentInstance) => {
-  // В этом методе описана логика обновления DOM, когда вызывается this.setState в компонентах
-
-  // Получаем текущий элемент oldVNode сохраненный в __vNode
-  const oldVNode = componentInstance.__vNode;
-  // Присваеваем обновленную версию DOM узла, вызывая метод render у переданного элемента
-  const newVNode = componentInstance.render();
-
-  // Обновляем __vNode свойство, заменив oldVNode обновленной версией newVNode
-  componentInstance.__vNode = reconcile(oldVNode, newVNode);
-}
-
-// Экспортируем функцию, чтоб использовать её как ReactDom.render
-const ReactDom = {
+const ReactDom =  {
   render
 };
 
