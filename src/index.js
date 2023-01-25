1093970d78b09bb71f2438d1a3e03444a19d4d95
Aleksandr Dronov <dronovspb@gmail.com>
2023-01-25 15:37:02 +0600

    function component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@
 import React from "./react";
 import ReactDom from "./react-dom";
 
+// Функциональный компонент
+const Greeting = ({ name }) => <p>Привет, {name}!</p>;
+
 const App = (
   <div>
     <h1 className="primary">
       Да здравствует React!
     </h1>
     <p>Самостоятельная реализация React в 90 строк кода</p>
+    <Greeting name={"Самодельный React"} />
   </div>
 );
 
--- src/react.js
+++ src/react.js
@@ -1,6 +1,10 @@
 import { h } from 'snabbdom';
 
 const createElement = (type, props = {}, ...children) => {
+    // Если type — функция, просто возвращаем результат этой функции.
+  if (typeof (type) == 'function') {
+    return type(props);
+  }
     return h(type, { props }, children);
   };
 
